fa50152741c0a1d3ec47963dda894a30674cc760
sergey leonov <redfortten@hotmail.com>
2021-10-25 23:28:13 +0300

    Добавьте файлы проекта.

---
+++ lab16.1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace lab16._1
+{
+    /* Разработать программу для записи информации о товаре в текстовый файл в формате json.
+    * Разрабоать класс для моделирования объекта "Товар". Предусмотреть члены класса "Код товара" (целое число), "Название товара" (строка), "Цена товара" (вещественное число).
+    * Создать массив из пяти товаров, значения должны вводиться пользователем с клавиатуры.
+    * Сериализовать массив в json-строку, сохранить её программно в файл "Products.json".*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Products.json";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }            
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            Product[] catalog = new Product[5];
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    catalog[i] = new Product();
+                    catalog[i].Input();
+                    Console.WriteLine();
+                }
+                string jsonString = JsonSerializer.Serialize(catalog, options);
+                sw.WriteLine(jsonString);
+            }            
+            Console.WriteLine("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        [JsonPropertyName("itemCode")]
+        public int ItemCode { get; set; }
+        [JsonPropertyName("itemName")]
+        public string ItemName { get; set; }
+        [JsonPropertyName("itemPrice")]
+        public double ItemPrice { get; set; }
+
+        public void Input()
+        {
+            try
+            {
+                Console.Write("Введите код товара (целое число): ");
+                ItemCode = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите название товара: ");
+                ItemName = Console.ReadLine();
+                Console.Write("Введите цену товара: ");
+                ItemPrice = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ lab16.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace lab16._2
+{
+    /* Разработать программу для получения информации о товаре из json-файла.
+     * Десериализовать файл "Products.json" из задачи lab16.1. Определить название самого дорогого товара.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double max = 0;
+            string itemName = "";
+            string path = "C:\\Users\\f0rtt\\source\\repos\\lab16\\lab16.1\\bin\\Debug\\Products.json";
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            Product[] catalog = JsonSerializer.Deserialize<Product[]>(File.ReadAllText(path));
+            for (int i = 0; i < catalog.Length; i++)
+            {                
+                Console.WriteLine("Код товара: {0} \nНазвание товара: {1} \nЦена товара: {2}", catalog[i].ItemCode, catalog[i].ItemName, catalog[i].ItemPrice);
+                Console.WriteLine();
+                if (catalog[i].ItemPrice > max)
+                {
+                    itemName = catalog[i].ItemName;
+                }
+            }
+            Console.WriteLine("Самый дорогой товар - {0}", itemName);            
+            Console.WriteLine("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        [JsonPropertyName("itemCode")]
+        public int ItemCode { get; set; }
+        [JsonPropertyName("itemName")]
+        public string ItemName { get; set; }
+        [JsonPropertyName("itemPrice")]
+        public double ItemPrice { get; set; }
+    }
+}
